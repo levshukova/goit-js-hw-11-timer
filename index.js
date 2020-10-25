11583933d6b993e5c389dbcb388c739377853015
levshukova <olevshukova@gmail.com>
2020-10-25 21:18:40 +0200

    Adds rendering ans some other changes

--- index.js
+++ index.js
@@ -12,15 +12,15 @@ class CountdownTimer {
     this.start();
   }
   start() {
-    this.runTimer();
+    this.getRemainingTime();
     setInterval(() => {
-      this.runTimer();
+      this.getRemainingTime();
     }, 1000);
   }
   getRemainingTime() {
     const currentTime = Date.now();
     const remainingTime = this.targetDate - currentTime;
-    return remainingTime;
+    this.getTimeComponents(remainingTime);
   }
 
   getTimeComponents(time) {
@@ -30,20 +30,33 @@ class CountdownTimer {
     );
     const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
     const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
-    return { days, hours, mins, secs };
+    this.render(days, hours, mins, secs);
   }
 
   pad(value) {
     return String(value).padStart(2, "0");
   }
-  updateClockface({ days, hours, mins, secs }) {
-    refs.days.textContent = `${days}`;
-    refs.hours.textContent = `${hours}`;
-    refs.mins.textContent = `${mins}`;
-    refs.secs.textContent = `${secs}`;
-  }
-  runTimer() {
-    this.updateClockface(this.getTimeComponents(this.getRemainingTime()));
+
+  render(days, hours, mins, secs) {
+    refs.timer.innerHTML = `<div class="field">
+        <span class="value" data-value="days">${days}</span>
+        <span class="label">Days</span>
+    </div>
+
+    <div class="field">
+        <span class="value" data-value="hours">${hours}</span>
+        <span class="label">Hours</span>
+    </div>
+
+    <div class="field">
+        <span class="value" data-value="mins">${mins}</span>
+        <span class="label">Minutes</span>
+    </div>
+
+    <div class="field">
+        <span class="value" data-value="secs">${secs}</span>
+        <span class="label">Seconds</span>
+    </div>`;
   }
 }
 
@@ -51,25 +64,3 @@ const timer = new CountdownTimer({
   selector: "#timer-1",
   targetDate: new Date("Oct 31, 2020"),
 });
-
-// const render = (time) => {
-//   refs.timer.innerHTML = `<div class="field">
-//         <span class="value" data-value="days">${days}</span>
-//         <span class="label">Days</span>
-//     </div>
-
-//     <div class="field">
-//         <span class="value" data-value="${hours}">11</span>
-//         <span class="label">Hours</span>
-//     </div>
-
-//     <div class="field">
-//         <span class="value" data-value="${mins}">11</span>
-//         <span class="label">Minutes</span>
-//     </div>
-
-//     <div class="field">
-//         <span class="value" data-value="${secs}">11</span>
-//         <span class="label">Seconds</span>
-//     </div>`;
-// };
